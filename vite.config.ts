1da1a7c5482563e8106bb4dbdaf8490e87911538
ThomasPalamara <palamara.thomas@gmail.com>
2024-04-10 14:50:05 +0100

    change plugin + add redirects

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,10 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import Pages from 'vite-plugin-pages';
-import mix from 'vite-plugin-mix';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    Pages({ dirs: 'src/pages' }),
-    mix({
-      handler: './api.js',
-    }),
-  ],
-  // server: {
-  //   cors: false,
-  //   open: true,
-  //   origin: '*',
-  //   proxy: {
-  //     '/api': {
-  //       target: 'https://api.imagekit.io/v1',
-  //       changeOrigin: true,
-  //       secure: false,
-  //       ws: true,
-  //     },
-  //   },
-  // },
+  plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
 });
