698ae94412d027c9bc632250568ca0d04905c01d
ThomasPalamara <palamara.thomas@gmail.com>
2024-10-07 14:52:33 +0100

    fix language selector for mobile

--- src/components/Navigation/LanguageSelector.tsx
+++ src/components/Navigation/LanguageSelector.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useIsMobile } from '../../utils/hooks';
 
 const LanguageSelector = () => {
   const { i18n } = useTranslation();
   const currentLanguage = i18n.language;
   const languageOptions = ['en', 'fr'];
+
+  const isMobile = useIsMobile();
 
   const [showOptions, setShowOptions] = useState(false);
 
@@ -16,17 +19,20 @@ const LanguageSelector = () => {
   return (
     <div className="relative">
       <div
-        className="cursor-pointer"
+        className={`cursor-pointer ${isMobile && 'hidden'}`}
         onClick={() => setShowOptions(!showOptions)}
       >
-        <img src={`flags/${currentLanguage}.svg`} />
+        <img
+          style={{ maxWidth: '20px' }}
+          src={`flags/${currentLanguage}.svg`}
+        />
         <span>{currentLanguage.toUpperCase()}</span>
       </div>
 
       <div
-        className={`${
-          !showOptions && 'hidden'
-        } absolute left-1/2 transform -translate-x-1/2 flex gap-7 bg-white pt-2 pb-1 px-5 shadow rounded-full`}
+        className={`${showOptions || isMobile ? '' : 'hidden'}  ${
+          !isMobile && 'absolute left-1/2 transform -translate-x-1/2 shadow'
+        } flex gap-7 bg-white pt-2 pb-1 px-5 shadow rounded-full`}
       >
         {languageOptions.map((language) => (
           <button onClick={() => handleLanguageChange(language)}>
